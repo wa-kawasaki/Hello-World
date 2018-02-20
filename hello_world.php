e74d6bccadb0b646edeb19b4a69ea54fca29ef1d
Takahiro Kawasaki <kawasaki@world-amenity.co.jp>
2018-02-20 17:06:00 +0900

    練習用

--- hello_world.php
+++ hello_world.php
@@ -1,6 +1,6 @@
 <?php
-	echo "Hello World2!";
-	echo "Hello World3!";
-	echo "Hello World5!";
-	echo "Hello World4!";
+	echo "Hello World1!";
+	echo "Hello World1!";
+	echo "Hello World1!";
+	echo "Hello World1!";
 ?>
